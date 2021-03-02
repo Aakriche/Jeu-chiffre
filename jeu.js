20d8140f1e4eaea95bdb22eb77eb67b06102eb71
Amaury AKRICHE <32291662+Margulda@users.noreply.github.com>
2021-03-02 09:13:42 +0100

    Add files via upload

--- jeu.js
+++ jeu.js
@@ -1,45 +1,38 @@
-var nbrD = document.getElementById('propInp')
-nbrD.readOnly = true;
+var nbrD = document.getElementById('propInp') //User input number
+nbrD.readOnly = true; //input field, Default: readOnly
 
 
 
-
-
-
-
+// Function for new game
 function newJeu (){
 
-    alert('Choisissez un chiffre entre 0 et 100 et rentrez le dans "Proposition", vous avez 30 secondes seulement !')
+    alert('Choisissez un chiffre entre 0 et 100 et rentrez le dans "Proposition", vous avez 30 secondes seulement !') //New game alert
 
 
-    nbrX = Math.floor(Math.random() * 100);
-    var indiceInp = document.querySelector("#indInp");
-    var repInp = document.querySelector("#repInp");
-    var tryInp = document.querySelector("#tryInp");
-    var chances = 2;
-    var propo = document.querySelector('#propBtn');
-    var soluce = document.querySelector('#solBtn');
-    var divTimer = document.querySelector('#timer');
-
-    indiceInp.value="";
+    nbrX = Math.floor(Math.random() * 100); // Random number generation
+    var indiceInp = document.querySelector("#indInp"); // Input for clues
+    var repInp = document.querySelector("#repInp"); // Input for answers
+    var tryInp = document.querySelector("#tryInp"); // Input for number of tries
+    var chances = 2; //Var for chances
+    var propo = document.querySelector('#propBtn'); // Proposition Button
+    var soluce = document.querySelector('#solBtn'); // Solution Button
+    
+    //Clear all input and disable readOnly
+    indiceInp.value=""; 
     repInp.value="";
     tryInp.value="";
     nbrD.readOnly = false;
     
-
+    // Disable / enable buttons 
     document.querySelector('#solBtn').disabled = false;
     document.querySelector('#propBtn').disabled = false;
     document.querySelector('#newBtn').disabled = true;
 
 
-    // var timerTvar = function timerT() {
-    //     i=Number("0");
-    //     setInterval(function(){i++;divTimer.innerHTML=i; }, 1000);
-    // }
-
-
+    //Function for game ending
     function finDuGame() {
         
+        //Clear all
         indiceInp.value="";
         repInp.value="";
         tryInp.value="";
@@ -49,45 +42,48 @@ function newJeu (){
         document.querySelector('#propBtn').disabled = true;
         document.querySelector('#newBtn').disabled = false;
         nbrD.readOnly = true;
-        clearTimeout(t);
-        clearTimeout(t2);
+        clearTimeout(t); //reset timer for game ending
+        clearTimeout(t2); // reset visible timer
     
     }
 
 
 
-    var t = 0;
+    var t = 0; //Visible timer
     var c = 0;
-    var t2 = 0;
+    var t2 = 0; // Hidden timer
 
+    // Function for Visible timer
     function timedCount() {
         document.getElementById("timer").innerHTML = c;
         c = c + 1;
         t = setTimeout(timedCount, 1000);
     }
 
+    //Function for ending
     function stopCount() {
        
         alert("Temps écoulé, le nombre mystère était " + nbrX);
         finDuGame();
     }
 
+    //Function for hidden timer
     timedCount();
     t2 = setTimeout(stopCount, 30000);
 
 
  
-
+    // Function for 1 try
     function proposition(){
 
 
         nbrD = document.getElementById('propInp');
 
-
+        //if number between 0 and 100
         if ((parseInt(nbrD.value) > 0) && (parseInt(nbrD.value) < 100)) {
 
             
-
+            // If win
             if ((parseInt(nbrD.value) == nbrX)) {
 
             finDuGame();
@@ -97,7 +93,7 @@ function newJeu (){
             
 
             }
-
+            //if lose
             if (chances > 7 ) { 
 
                 finDuGame();
@@ -106,7 +102,7 @@ function newJeu (){
                 alert('perdu, le nombre mystère était : ' + nbrX)
                 
             }
-  
+            //If too high
             else if ((parseInt(nbrD.value) > nbrX)) {
 
             indiceInp.value = "Trop grand, c'est moins !";
@@ -116,7 +112,7 @@ function newJeu (){
             chances++;
 
             }
-
+            //if too low
             else if ((parseInt(nbrD.value) < nbrX)) {
 
             indiceInp.value = "Trop petit, c'est plus !";
@@ -130,7 +126,7 @@ function newJeu (){
         
 
         }
-
+        //If number NOt between 0 and 100
         else if ((parseInt(nbrD.value) < 0) || (parseInt(nbrD.value) >= 101)) {
 
             alert("Entre 0 et 100 on a dit");
@@ -145,26 +141,26 @@ function newJeu (){
 
     
 
-
+    //Function for clicking solution
     function solution(){
         alert('Le chiffre mystère était ' + nbrX);
         finDuGame();
     }
     
     
-    var soluce = document.querySelector('#solBtn');
-    soluce.addEventListener("click", solution);
-    soluce.addEventListener("click", finDuGame);
-    propo.addEventListener("click", proposition);
+    var soluce = document.querySelector('#solBtn'); // Solution Button
+    soluce.addEventListener("click", solution); // Clicking solution
+    soluce.addEventListener("click", finDuGame); // Clicking solution
+    propo.addEventListener("click", proposition); // Clicking Proposition
 
 
  
 }
 
 
-document.querySelector('#solBtn').disabled = true;
-document.querySelector('#propBtn').disabled = true;
+document.querySelector('#solBtn').disabled = true;  //Disable solution button out of a game
+document.querySelector('#propBtn').disabled = true; //Disable proposition button out of a game
 
 
-var newBtn = document.querySelector('#newBtn');
-newBtn.addEventListener("click", newJeu);
+var newBtn = document.querySelector('#newBtn'); // Button for New game
+newBtn.addEventListener("click", newJeu); // Clicking for a New Game
